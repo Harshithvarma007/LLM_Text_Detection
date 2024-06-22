d679c57a594476ddcc2e8c1dfef059160c4aac80
Harshithvarma007 <112501476+Harshithvarma007@users.noreply.github.com>
2024-06-22 12:22:35 +0530

    Model Training Done

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
 from LLM_text_detection import logger
-
+from LLM_text_detection.utils.common import *
+from LLM_text_detection.constants import *
+read_yaml(CONFIG_FILE_PATH)
 logger.info("Welcome to LLM Logger")
+
--- src/LLM_text_detection/utils/common.py
+++ src/LLM_text_detection/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from wine_quality_prediction import logger
+from LLM_text_detection import logger
 import json
 import joblib
 from ensure import ensure_annotations
